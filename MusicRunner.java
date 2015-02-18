e53a99df0946e2ed5f81bd72b1486292b6fd48a5
caffreyt <tannercaf@gmail.com>
2015-02-18 10:06:46 -0500

    Part 1 Done
    dammit tanczos

--- MusicRunner.java
+++ MusicRunner.java
@@ -1,9 +1,15 @@
 import java.util.Arrays;
+import java.util.*;
 
 public class MusicRunner
 {
+  
+  public static String fuckyoutanczos(String input)
+  {return input.substring(1,input.length()-1);}
+  
   public static void main (String[] args)
   {
+    ArrayList<Song> Songs = new ArrayList<Song>();
     int count = 0;
     MusicReader mr = new MusicReader();
     
@@ -13,7 +19,7 @@ public class MusicRunner
     
     // First line contains all the fields - We don't want to save this anywhere but we can
     // print it for now to see what information we have.
-    System.out.println(Arrays.toString(data));
+    // System.out.println(Arrays.toString(data));
     
     data = mr.getSongData();  // Get next line of song data
     
@@ -23,17 +29,26 @@ public class MusicRunner
     while (data != null)
     {
       // You probably will comment this out but for now print out the line so you can see what is there
-      System.out.println(Arrays.toString(data));
+      
       
       // Let's try to create a Song object
-      Song song = new Song(data[0], data[1]);  // data[0] is the artist and data[1] is the name
-      
+      Song song = new Song(data[0], data[1], Integer.parseInt(fuckyoutanczos(data[3])), Double.parseDouble(fuckyoutanczos(data[4])), data[16]);  // data[0] is the artist and data[1] is the name
+     if(fuckyoutanczos(data[2]).equals("song"))
+     {
+       Songs.add(song);
       count++;
-      
-      if (count == 1)  // For now only read ONE song
+     } 
+      if (count == 10)  // For now only read ONE song
         break;
       
       data = mr.getSongData();  // Get next line of song data
+    }
+    
+    for(int i = Songs.size() - 1; i >=0; i--)
+    {
+      Song song = Songs.get(i);
+      System.out.println(song.artist + " " + song.name); 
+      
     }
     
     mr.close();
